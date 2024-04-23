02f3df00e902f535761ff5d67e58d9c668b3bb3c
OmarHani4306 <109454477+OmarHani4306@users.noreply.github.com>
2024-04-24 01:58:17 +0200

    Update Main.java
    added number of deletions and insertions

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,20 +4,23 @@ import java.util.List;
 import java.util.Random;
 
 public class PerfectHashing<T> {
-    private static final int N = 100; //N is the size of the dictionary 
+    private static final int N = 100; // N is the size of the dictionary
     private static final Random random = new Random();
 
     private List<T>[] firstLevelTable;
     private List<T>[] secondLevelTables;
+    private int numberOfInsertions;
+    private int numberOfDeletions;
 
     public PerfectHashing() {
-        
         this.firstLevelTable = new List[N];
         for (int i = 0; i < N; i++) {
             this.firstLevelTable[i] = new ArrayList<>();
         }
 
         this.secondLevelTables = new List[N];
+        this.numberOfInsertions = 0;
+        this.numberOfDeletions = 0;
     }
 
     private <T> int universalHash(T key, int tableSize) {
@@ -25,25 +28,36 @@ public class PerfectHashing<T> {
         int hashCode = key.hashCode();
         int a = random.nextInt(tableSize - 1) + 1; // Random coefficient a in the range [1, tableSize-1] to ensure that `a` mod `tableSize` is a non-zero value 
         int b = random.nextInt(tableSize);         // Random constant b in the range [0, tableSize-1] to cover all possible offset values
-        
         return ((a * hashCode + b) % 101) % tableSize; 
     }
 
-    public void insert(T key) {
+    public boolean insert(T key) {
         int hash1 = universalHash(key, N);
-        if (this.secondLevelTables[hash1] == null) {
-            this.firstLevelTable[hash1].add(key);
-        } else {
-            int hash2 = universalHash(key, this.secondLevelTables[hash1].size());
-            this.secondLevelTables[hash1].set(hash2, key);
+        if (!this.firstLevelTable[hash1].contains(key)) {
+            if (this.secondLevelTables[hash1] == null) {
+                this.firstLevelTable[hash1].add(key);
+                numberOfInsertions++;
+                return true;
+            } else {
+                int hash2 = universalHash(key, this.secondLevelTables[hash1].size());
+                this.secondLevelTables[hash1].set(hash2, key);
+                numberOfInsertions++;
+                return true;
+            }
         }
+        return false;
     }
 
-    public void delete(T key) {
+    public boolean delete(T key) {
         int hash1 = universalHash(key, N);
-        if (this.secondLevelTables[hash1] == null || !this.secondLevelTables[hash1].remove(key)) {
-            this.firstLevelTable[hash1].remove(key);
+        if (this.firstLevelTable[hash1].contains(key)) {
+            if (this.secondLevelTables[hash1] == null || !this.secondLevelTables[hash1].remove(key)) {
+                this.firstLevelTable[hash1].remove(key);
+            }
+            numberOfDeletions++;
+            return true;
         }
+        return false;
     }
 
     public boolean search(T key) {
@@ -76,7 +90,16 @@ public class PerfectHashing<T> {
             }
         }
     }
+
+    public int getNumberOfInsertions() {
+        return numberOfInsertions;
+    }
+
+    public int getNumberOfDeletions() {
+        return numberOfDeletions;
+    }
 }
+
 
 public class Main {
     public static void main(String[] args) {
